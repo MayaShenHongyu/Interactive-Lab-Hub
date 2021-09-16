bc6bdcd6e2e6d027963a089b170171f9286a7ac2
Maya Shen <hs45@rice.edu>
2021-09-16 10:21:11 -0400

    Update image.py

--- Lab 2/image.py
+++ Lab 2/image.py
@@ -81,21 +81,58 @@ backlight.value = True
 
 
 # Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+# image_ratio = image.width / image.height
+# screen_ratio = width / height
+# if screen_ratio < image_ratio:
+#     scaled_width = image.width * height // image.height
+#     scaled_height = height
+# else:
+#     scaled_width = width
+#     scaled_height = image.height * width // image.width
+# image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
+# # Crop and center the image
+# x = scaled_width // 2 - width // 2
+# y = scaled_height // 2 - height // 2
+# image = image.crop((x, y, x + width, y + height))
 
 # Display image.
+# disp.image(image)
+
+
+
+
+######## Buttons
+
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+def rescale_image(image):
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+    return image
+
+image = rescale_image(image)
+image2 = rescale_image(image2)
+
 disp.image(image)
 
+while True:
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        disp.image(image)
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        disp.image(image2)
