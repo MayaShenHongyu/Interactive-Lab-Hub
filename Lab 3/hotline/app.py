90299670d19f0802db18011757740825c4bebe44
Maya Shen <hs45@rice.edu>
2021-10-12 20:15:12 -0400

    x

--- Lab 3/hotline/app.py
+++ Lab 3/hotline/app.py
@@ -4,8 +4,20 @@ import os
 import wave
 import json
 
-cmd = "arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav user_input.wav"
-subprocess.call(cmd, shell=True)
+
+
+def speak(instruction):
+    command = """
+        say() { 
+            local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; 
+        } ; 
+    """ + f"say '{instruction}'"
+    subprocess.call(command, shell=True)
+
+speak("say hello!")
+
+record_user_input_cmd = "arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav user_input.wav"
+subprocess.call(record_user_input_cmd, shell=True)
 
 if not os.path.exists("../model"):
     print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
@@ -27,5 +39,7 @@ while True:
     if rec.AcceptWaveform(data):
         res = json.loads(rec.Result())
         print("Result:", res['text'])
+    else:
+        print(rec.PartialResult())
 
 
