30e1e876dcc79bf0c2b6948dff21044789e1dd8b
Maya Shen <hs45@rice.edu>
2021-10-25 17:44:15 -0400

    x

--- Lab 4/animal_band/app.py
+++ Lab 4/animal_band/app.py
@@ -21,4 +21,12 @@ while True:
     for i in range(12):
         if mpr121[i].value:
             print(f"Twizzler {i} touched!")
-    time.sleep(0.25)  # Small delay to keep from spamming output messages.
+    if mpr121[yellow].value: 
+        subprocess.call("aplay llama.wav", shell=True)
+    elif mpr121[green].value: 
+        subprocess.call("aplay bear.wav", shell=True)
+    elif mpr121[red].value: 
+        subprocess.call("aplay fox.wav", shell=True)
+    elif mpr121[white].value: 
+        subprocess.call("aplay hello.wav", shell=True)
+    # time.sleep(0.25)  # Small delay to keep from spamming output messages.
