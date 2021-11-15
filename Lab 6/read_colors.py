73fad704b2c7d6eb279da001a0bcfc520f1fc607
Maya Shen <hs45@rice.edu>
2021-11-15 18:48:46 -0500

    x

---
+++ Lab 6/read_colors.py
@@ -0,0 +1,96 @@
+import paho.mqtt.client as mqtt
+import uuid
+import board
+import busio
+import adafruit_apds9960.apds9960
+import time
+import paho.mqtt.client as mqtt
+import uuid
+import signal
+
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+height =  disp.height
+width = disp.width 
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+
+# the # wildcard means we subscribe to all subtopics of IDD
+topic = 'IDD/colors'
+
+# some other examples
+# topic = 'IDD/a/fun/topic'
+
+#this is the callback that gets called once we connect to the broker. 
+#we should add our subscribe functions here as well
+def on_connect(client, userdata, flags, rc):
+	print(f"connected with result code {rc}")
+	client.subscribe(topic)
+	# you can subsribe to as many topics as you'd like
+	# client.subscribe('some/other/topic')
+
+
+# this is the callback that gets called each time a message is recived
+def on_message(cleint, userdata, msg):
+	print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
+	# you can filter by topics
+	# if msg.topic == 'IDD/some/other/topic': do thing
+	message = msg.payload.decode('UTF-8')
+	color = [int(x) for x in message.split(",")]
+    draw.rectangle((0, height*0.5, width, height), fill=color[:3])
+    disp.image(image)
+    time.sleep(.01)
+
+# Every client needs a random ID
+client = mqtt.Client(str(uuid.uuid1()))
+# configure network encryption etc
+client.tls_set()
+# this is the username and pw we have setup for the class
+client.username_pw_set('idd', 'device@theFarm')
+
+# attach out callbacks to the client
+client.on_connect = on_connect
+client.on_message = on_message
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+# this is blocking. to see other ways of dealing with the loop
+#  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
+client.loop_forever()
