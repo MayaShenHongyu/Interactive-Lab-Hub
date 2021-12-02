3294111c5fec2c68ee066d75d90a4d4abe24b3be
Maya Shen <hs45@rice.edu>
2021-12-01 21:23:10 -0500

    x

---
+++ Final/app.py
@@ -0,0 +1,57 @@
+from vosk import Model, KaldiRecognizer
+import subprocess
+import os
+import wave
+import json
+
+if not os.path.exists("../model"):
+    print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
+    exit (1)
+
+USER_INPUT_FILE = "user_input.wav"
+model = Model("../model")
+
+def speak(instruction):
+    command = """
+        say() { 
+            local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; 
+        } ; 
+    """ + f"say '{instruction}'"
+    subprocess.call(command, shell=True)
+
+def dont_understand():
+    speak("What, what are you saying? I don’t understand.")
+
+def record_user_input():
+    subprocess.call("arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav " + USER_INPUT_FILE, shell=True)
+
+
+def recognize(pattern):
+    wf = wave.open(USER_INPUT_FILE, "rb")
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print ("Audio file must be WAV format mono PCM.")
+        exit (1)
+
+    rec = KaldiRecognizer(model, wf.getframerate(), pattern)
+
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            res = json.loads(rec.Result())
+            print("Result:", res['text'])
+            return res['text']
+        # else:
+        #     print(rec.PartialResult())
+    print("Failed to recognize")
+    return ""
+
+speak("Record now:")
+record_user_input()
+r = recognize("key [unk]")
+print(r)
+speak("Record now:")
+record_user_input()
+r = recognize("key")
+print(r)
