9d85831a01c786b62ba857a792ca5fa2fbd6d3d5
Maya Shen <hs45@rice.edu>
2021-12-12 20:37:08 -0500

    x

--- Final/test1.py
+++ Final/test1.py
@@ -11,6 +11,7 @@ except:
 
 ret,frame = cap.read() # return a single frame in variable `frame`
 while(True):
+    cv2.imwrite('images/c2.png',frame)
     cv2.imshow('img1',frame) #display the captured image
     if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
         cv2.imwrite('images/c1.png',frame)
