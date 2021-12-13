900dd2b6d385e89bc81fe294e83c4cf439d00215
Maya Shen <hs45@rice.edu>
2021-12-12 20:45:26 -0500

    x

--- Final/test1.py
+++ Final/test1.py
@@ -16,7 +16,7 @@ while(True):
     # cv2.imwrite('images/c2.png',frame)
     cv2.imshow('img1',frame) #display the captured image
     if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
-        cv2.imwrite('images/c1.png',frame)
+        cv2.imwrite(os.getcwd() + '/c1.png',frame)
         cv2.destroyAllWindows()
         break
 
